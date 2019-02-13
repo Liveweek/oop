0daa661665906933de5b2a1481bd4735ab74d924
Unknown <konakov.po@yandex.ru>
2019-02-13 20:56:36 +0300

    praktice 3 change

--- prak3_3.cpp
+++ prak3_3.cpp
@@ -1,49 +1,14 @@
 #include <string>
 #include <iostream>
-#include <vector>
 using namespace std;
-class Book {
-    public:
-        string name;
-        string author;
-        int year;
-        int count_of_pages;
-        Book() {
-            cin.get();
-            cout << "Введите имя автора: " << '\n';
-            getline(std::cin,author);
-            cout << "Введите название книги: "<< '\n';
-            getline(std::cin,name);
-            cout << "Год: " << '\n';
-            cin >> year;
-            cout << "Кол-во страниц: " << '\n';
-            cin >> count_of_pages;
-        }
-};
-
-class Bibl {
-    public:
-        int year;
-        int count_of_books;
-        Book *books;
-        Bibl () {
-            cout << "Введите год основания: ";
-            cin >> year;
-            cout << "Введите количество книг в библиотеке: ";
-            cin >> count_of_books;
-            books = new Book[count_of_books];
-        }
+struct Book {
+    int year;
+    int count_of_pages;
+    string name;
+    string author;
 };
 
 int main() {
-    Book b1;
-    Bibl biblio;
-    cout << "Все книги библиотеки (" << biblio.count_of_books << "):";
-    for (int i = 0; i < biblio.count_of_books; i++) {
-        cout << biblio.books[i].author << '\n';
-        cout << biblio.books[i].name << '\n';
-        cout << "Год: " << biblio.books[i].year << '\n';
-        cout << "Количество страниц: " << biblio.books[i].count_of_pages << '\n';
-    }
+    Book book1 = {1999, 2000,(string)"Лолек и болек",(string)"Мудак полный"};
     return 0;
 }
