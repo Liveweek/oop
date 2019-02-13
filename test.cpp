5781cfa8c7f0b312d827d29bfedba95122deed0f
Unknown <konakov.po@yandex.ru>
2019-02-13 09:18:23 +0300

    firstcommit

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    string name;
+    cout << "Введите ваше имя:";
+    cin >> name;
+    cout << "Привет, " << name;
+    return 0;
+}
