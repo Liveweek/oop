1a3ec30f8ea31cb40e10c3e1e7a390942e3cb8b5
Unknown <konakov.po@yandex.ru>
2019-02-15 22:31:35 +0300

    prak5 4 final

--- 5/prak5_4.cpp
+++ 5/prak5_4.cpp
@@ -3,23 +3,56 @@
 #include <set>
 using namespace std;
 
-string proiz(string a, string b) {
+
+
+
+template <typename T>
+T summ(T a, T b) {
+    return a + b;
+}
+
+string summ(string a, string b) {
+    return a + b;
+}
+
+string summ(char a, char b) {
+    string s;
+    s.push_back(a);
+    s.push_back(b);
+    return s;
+}
+
+
+template <typename T>
+T mins(T a, T b) {
+    return a - b;
+}
+
+string mins(string a, string b) {
+    string s;
     multiset<char> str_a;
     multiset<char> str_b;
+    for (int i = 0; i < a.size();i++) {
+        str_a.insert(a[i]);
+    }
     for (int i = 0; i < b.size();i++) {
         str_b.insert(b[i]);
     }
-    for (int i = 0; i < a.size(); i++) {
-        if(!str_b.count(a[i])) {
-            str_a.insert(a[i]);
+    for (int i = 0; i < a.size(); i++ ) {
+        if (str_b.count(a[i])) {
             str_b.erase(a[i]);
+            str_a.erase(a[i]);
         }
     }
-    string s;
     for (auto e = str_a.begin(); e != str_a.end(); e++) {
         s.push_back(*e);
     }
     return s;
+}
+
+template <typename T>
+T proiz(T a, T b) {
+    return a * b;
 }
 
 string proiz(char a, int b) {
@@ -38,56 +71,6 @@ string proiz(string a, int b) {
         }
     }
     return s;
-}
-
-string mins(string a, string b) {
-    string s;
-    multiset<char> str_a;
-    multiset<char> str_b;
-    for (int i = 0; i < a.size();i++) {
-        str_a.insert(a[i]);
-    }
-    for (int i = 0; i < b.size();i++) {
-        str_b.insert(b[i]);
-    }
-    for (int i = 0; i < a.size(); i++ ) {
-        if (!str_b.count(a[i])) {
-            str_b.erase(a[i]);
-            str_a.erase(a[i]);
-        }
-    }
-    for (auto e = str_a.begin(); e != str_a.end(); e++) {
-        s.push_back(*e);
-    }
-    return s;
-}
-
-string summ(string a, string b) {
-    return a + b;
-}
-
-string summ(char a, char b) {
-    string s;
-    s.push_back(a);
-    s.push_back(b);
-    return s;
-}
-
-
-
-template <typename T>
-T summ(T a, T b) {
-    return a + b;
-}
-
-template <typename T>
-T mins(T a, T b) {
-    return a - b;
-}
-
-template <typename T>
-T proiz(T a, T b) {
-    return a * b;
 }
 
 template <typename T>
@@ -127,21 +110,21 @@ int main() {
     cout << "сумма: " << '\t' << summ(int_a,int_b) << endl;
     cout << "разность: " << '\t' << mins(int_a,int_b) << endl;
     cout << "произведение: " << '\t' << proiz(int_a,int_b) << endl;
-    // cout << "деление: " << '\t' << del(int_a,int_b) << endl;
+    cout << "деление: " << '\t' << del(int_a,int_b) << endl;
     cout << "===============" << endl << endl<<endl;
     cout << "float: " << '\t' << f_a << '\t' << f_b << endl;
     cout << "===============" << endl;
     cout << "сумма: " << '\t' << summ(f_a,f_b) << endl;
     cout << "разность: " << '\t' << mins(f_a,f_b) << endl;
     cout << "произведение: " << '\t' << proiz(f_a,f_b) << endl;
-    // cout << "деление: " << '\t' << del(f_a,f_b) << endl;
+    cout << "деление: " << '\t' << del(f_a,f_b) << endl;
     cout << "===============" << endl << endl<<endl;
     cout << "double: " << '\t' << d_a << '\t' << d_b << endl;
     cout << "===============" << endl;
     cout << "сумма: " << '\t' << summ(d_a,d_b) << endl;
     cout << "разность: " << '\t' << mins(d_a,d_b) << endl;
     cout << "произведение: " << '\t' << proiz(d_a,d_b) << endl;
-    // cout << "деление: " << '\t' << del(d_a,d_b) << endl;
+    cout << "деление: " << '\t' << del(d_a,d_b) << endl;
     cout << "===============" << endl << endl<<endl;
     cout << "char: " << '\t' << ch_a << '\t' << ch_b << endl;
     cout << "===============" << endl;
@@ -152,9 +135,7 @@ int main() {
     cout << "===============" << endl;
     cout << "сумма: " << '\t' << summ(str_a,str_b) << endl;
     cout << "разность: "<< '\t' << mins(str_a,str_b) << endl;
-    cout << "произведение строки "<< '\t'<< str_a<<" на целое число "<< int_a << '\t' << proiz(str_a,int_a);
-    cout << "произведение (пересечение множеств): " << '\t' << proiz(str_a,str_b) << endl;
+    cout << "произведение строки "<< '\t'<< str_a<<" на целое число "<< int_a << '\t' << proiz(str_a,int_a) << endl;;
     cout << "===============" << endl << endl<<endl;
-
-
+    return 0;
 }
